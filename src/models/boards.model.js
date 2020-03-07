020033c09a7bd681f2e30495bfd6685fea2f4d8b
y-kawamura <tetora.y@gmail.com>
2020-03-07 15:57:55 +0900

    Modify boards model

--- src/models/boards.model.js
+++ src/models/boards.model.js
@@ -7,7 +7,10 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const schema = new Schema({
-    text: { type: String, required: true }
+    name: { type: String, required: true },
+    background: { type: String, required: true },
+    ownerId: { type: Schema.Types.ObjectId, ref: 'users' },
+    memberId: [{ type: Schema.Types.ObjectId, ref: 'users' }],
   }, {
     timestamps: true
   });
