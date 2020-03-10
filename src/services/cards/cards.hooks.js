e3abeec5b42433a9530ba720973d1a0ccda5e762
y-kawamura <tetora.y@gmail.com>
2020-03-10 17:25:02 +0900

    Restrict to access lists and cards to board owner

--- src/services/cards/cards.hooks.js
+++ src/services/cards/cards.hooks.js
@@ -1,14 +1,59 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const mongoose = require('mongoose');
+
+const getListIdByCardId = async (cardId) => {
+  const cards = mongoose.model('cards');
+  try {
+    const card = await cards.findOne({ _id: cardId });
+    if (card === null) {
+      return Promise.reject(new Error('card is not found'));
+    }
+    return card.listId;
+  } catch (error) {
+    return Promise.reject(new Error('card is not found'));
+  }
+};
+
+const isBoardOwner = async (context, listId) => {
+  const lists = mongoose.model('lists');
+  const list = await lists.findOne({ _id: listId });
+  const boardId = list.boardId;
+
+  const boards = mongoose.model('boards');
+  const board = await boards.findOne({ _id: boardId });
+
+  if (board.ownerId.toString() === context.params.user._id.toString()) {
+    return context;
+  } else {
+    return Promise.reject(new Error('Un-Authorized'));
+  }
+};
+
+const isBoardOwnerById = async (context) => {
+  const listId = await getListIdByCardId(context.id);
+  await isBoardOwner(context, listId);
+};
+
+const isBoardOwnerByData = async (context) => {
+  const listId = context.data.listId;
+  await isBoardOwner(context, listId);
+};
+
+const isBoardOwnerByQuery = async (context) => {
+  const listId = context.params.query.listId;
+  await isBoardOwner(context, listId);
+};
+
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    all: [ authenticate('jwt'), ],
+    find: [ isBoardOwnerByQuery ],
+    get: [ isBoardOwnerById ],
+    create: [ isBoardOwnerByData ],
+    update: [ isBoardOwnerById ],
+    patch: [ isBoardOwnerById ],
+    remove: [ isBoardOwnerById ]
   },
 
   after: {
--- src/services/lists/lists.hooks.js
+++ src/services/lists/lists.hooks.js
@@ -1,14 +1,55 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const mongoose = require('mongoose');
+
+const getBoardIdByListId = async (listId) => {
+  const lists = mongoose.model('lists');
+  try {
+    const list = await lists.findOne({ _id: listId });
+    if (list === null) {
+      return Promise.reject(new Error('list is not found'));
+    }
+    return list.boardId;
+  } catch (error) {
+    return Promise.reject(new Error('list is not found'));
+  }
+};
+
+const isBoardOwner = async (context, boardId) => {
+  const boards = mongoose.model('boards');
+  const board = await boards.findOne({ _id: boardId });
+
+  if (board.ownerId.toString() === context.params.user._id.toString()) {
+    return context;
+  } else {
+    return Promise.reject(new Error('Un-Authorized'));
+  }
+};
+
+const isBoardOwnerById = async (context) => {
+  const boardId = await getBoardIdByListId(context.id);
+  await isBoardOwner(context, boardId);
+};
+
+const isBoardOwnerByData = async (context) => {
+  const boardId = context.data.boardId;
+  await isBoardOwner(context, boardId);
+};
+
+const isBoardOwnerByQuery = async (context) => {
+  const boardId = context.params.query.boardId;
+  await isBoardOwner(context, boardId);
+};
+
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    find: [ isBoardOwnerByQuery ],
+    get: [ isBoardOwnerById ],
+    create: [ isBoardOwnerByData ],
+    update: [ isBoardOwnerById ],
+    patch: [ isBoardOwnerById ],
+    remove: [ isBoardOwnerById ]
   },
 
   after: {
